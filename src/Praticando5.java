fd522b076bbfd3d8043a4a461ac8e139c83077a8
Walacy Pereira <wollygrin@gmail.com>
2026-09-13 23:43:56 -0300

    Criar Praticando5.java

---
+++ src/Praticando5.java
@@ -0,0 +1,13 @@
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Somando pares do 1 atê 100");
+		int soma = 0;
+		for(int i = 0; i <= 100; i++){
+            if(i % 2 == 0){
+            	soma = soma + i;
+            }
+		}
+		System.out.println("Resultado da soma: " +soma);
+	}
+}
+
