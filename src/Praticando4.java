55126f8db9da597b0cc467415b889d7661dec51b
Walacy Pereira <wollygrin@gmail.com>
2026-09-13 23:31:39 -0300

    Criar Praticando4.java

---
+++ src/Praticando4.java
@@ -0,0 +1,15 @@
+public class Main {
+	public static void main(String[] args) {
+	
+		int[] arrayNum = {25,34,90};
+		int maiorNum = arrayNum[0];
+		 
+		 for(int i = 0; i < arrayNum.length; i++){
+		if(arrayNum[i] > maiorNum){
+			maiorNum = arrayNum[i];
+		}
+	}
+		System.out.println("Maior numero: " + maiorNum);
+	}
+}
+
