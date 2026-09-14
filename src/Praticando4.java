c21e3f04115cc30cf20a01689575d3ef0db8f918
Walacy Pereira <wollygrin@gmail.com>
2026-09-13 23:32:39 -0300

    Atualizar o Praticando4.java

--- src/Praticando4.java
+++ src/Praticando4.java
@@ -2,9 +2,11 @@ public class Main {
 	public static void main(String[] args) {
 	
 		int[] arrayNum = {25,34,90};
+
+ //assumindo que o primeiro é maior
 		int maiorNum = arrayNum[0];
 		 
-		 for(int i = 0; i < arrayNum.length; i++){
+	for(int i = 0; i < arrayNum.length;i++){
 		if(arrayNum[i] > maiorNum){
 			maiorNum = arrayNum[i];
 		}
