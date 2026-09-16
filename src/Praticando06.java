dac7087938a841020fbaaa2e8c155f3a03a7cac4
Walacy Pereira <wollygrin@gmail.com>
2026-09-16 17:54:48 -0300

    Update Praticando06.java

--- src/Praticando06.java
+++ src/Praticando06.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Main {
+public class Praticando06 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("### - Tabuada - ###");
